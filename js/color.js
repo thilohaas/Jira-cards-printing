ad15cbdc306a09c59a1773210de780ca760c0bdd
Peter Manser <mail@petermanser.ch>
2014-11-25 21:31:18 +0100

    Fix.

--- js/color.js
+++ js/color.js
@@ -3,7 +3,7 @@ var getRgbaColor = function(colorString) {
         colorStringShortened = colorString.substr(0, 1) + colorString.substr(-2, 2);
 
     for (var i = 0; i < 3; i++) {
-        rgb.push(parseInt((colorStringShortened.charCodeAt(i % colorString.length) * colorString.length) % 255));
+        rgb.push(parseInt((colorStringShortened.charCodeAt(i % colorStringShortened.length) * colorString.length) % 255));
     }
 
     return 'rgba(' + rgb.join(',') + ',0.7)';
